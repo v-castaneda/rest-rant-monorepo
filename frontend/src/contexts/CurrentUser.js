b6a9b8a1baed591c9b343d8e1fa9eb85fea946ab
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-11-14 23:05:22 -0500

    including JWT in fetch request

--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -8,7 +8,11 @@ function CurrentUserProvider({ children }){
     const [currentUser, setCurrentUser] = useState(null)
     useEffect(() => {
         const getLoggedInUser = async () => {
-            let response = await fetch('http://localhost:5000/authentication/profile')
+            let response = await fetch('http://localhost:5000/authentication/profile', {
+                headers: {
+                    'Authorization': `Bearer ${localStorage.getItem('token')}`
+                }
+            })
             let user = await response.json()
             setCurrentUser(user)
         }

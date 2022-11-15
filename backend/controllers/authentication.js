2b35a35ca80367843e829606b2819c994dedc7df
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-11-14 19:43:37 -0500

    scripting shell of authentication controller

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -20,4 +20,17 @@ router.post('/', async (req, res) => {
 
 })
 
+router.get('/profile', async (req, res) => {
+    // try {
+    //     let user = await User.findOne({
+    //         where: {
+    //             userId: __
+    //         }
+    //     })
+    //     res.json(user)
+    // } catch {
+    //     res.json(null)
+    // }
+})
+
 module.exports = router
